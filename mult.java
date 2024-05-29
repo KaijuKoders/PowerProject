274b7130add78849cc3406e18aeffba08c14097e
KaijuKoders <avishkar.yelave@nciportal.com>
2024-05-29 14:30:13 +0530

    add new feauture 1

--- mult.java
+++ mult.java
@@ -1,11 +1,19 @@
 
 public class mult 
 {
+	void sub(int a,int b)
+	{
+		System.out.println(a - b);
+	}
 	public static void main(String[] args) 
 	{
 		int a = 5;
 		int b = 6;
 		
 		System.out.println(a*b);
+		
+		mult m = new mult();
+		m.sub(a, b);
+		
 	}
 }
